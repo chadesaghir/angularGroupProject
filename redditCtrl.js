e5c9c4e47869ddf7acfa2af3421b559c8ee8ca48
Katherine Hyland <katherinelizabethyland@gmail.com>
2017-11-15 20:49:12 -0500

    added reddit pages

--- redditCtrl.js
+++ redditCtrl.js
@@ -1,2 +1,11 @@
 var app = angular.module('tacoCat');
-app.controller ('redditController')
+app.controller('redditController', function($scope, $timeout, redditFactory){
+  // $scope.animal= "dog"; (scope add'l things to view)
+  
+   redditFactory.getPosts().then(function(data){
+     console.log(data);
+     $timeout($scope.posts = data);
+   });
+ 
+ 
+ });
