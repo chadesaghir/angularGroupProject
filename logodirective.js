e6b8087d2f3ee38891e144dad4fb35ac8c816cbd
Katherine Hyland <katherinelizabethyland@gmail.com>
2017-11-16 12:38:24 -0500

    more changes

--- logodirective.js
+++ logodirective.js
@@ -6,3 +6,10 @@ app.directive('logo', function(){
     template: '<img src="topbar.png">'
   }
 });
+app.directive('tacoCatGif', function(){
+  return {
+    restrict: 'E',
+    replace: false,
+    template: '<img src="https://vignette.wikia.nocookie.net/r2d/images/c/c4/Taco_Cat.gif/revision/latest?cb=20150522024654">'
+  }
+});
--- palindromeCtrl.js
+++ palindromeCtrl.js
@@ -1,7 +1,9 @@
 var app = angular.module('tacoCat');
 app.controller('palindromeController', function ($scope) {
-    $scope.palicheck = function (word) {
-        var revword = word.split('').reverse().join('');
-        return revword == word;
-    };
+   
+   $scope.palicheck = function palidrome(word) {
+       var newStr=word.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+       var revword = newStr.split('').reverse().join('');
+       return revword == newStr;
+   };
 });
